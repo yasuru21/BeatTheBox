593e63a5ea1451d10550c62048b1ae89f51531c1
ssakthi <ssakthi@iu.edu>
2020-11-09 18:55:06 -0500

    PlayableStack and DeadStack completed

--- DeadStack.py
+++ DeadStack.py
@@ -1,5 +1,23 @@
 #container that stores cards in a dead stack
 
 class DeadStack:
+    Stack c = []
+    def __init__(self, unplayable):
+        c = flipStack(unplayable)
 
-    def __init__(self):
+    def flipStack(Stack):
+        if not isEmpty(Stack):
+            temp = pop(Stack)
+            flipStack(Stack)
+            insertAtBottom(Stack, temp)
+
+    def insertAtBottom(stack, i):
+        if isEmpty(stack):
+            push(stack, i)
+        else:
+            temp = pop(stack)
+            insertAtBottom(stack, i)
+            push(stack, temp)
+
+    def getStatus():
+        return 1
--- PlayableStack.py
+++ PlayableStack.py
@@ -1,9 +1,14 @@
 #container to store cards in a playable stack
 
 class PlayableStack:
-    
+    Stack c = []
     def __init__(self, cards):
+        Stack c = cards
 
-    def addCard(Deck):
+
+    def addCard(Deck k):
         #takes card from top of Deck and stores it
+        c.append(k.drawCard())
 
+    def getStatus():
+        return 0
