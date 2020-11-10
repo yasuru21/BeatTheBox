2f748ebbb8e47eaf989259517a257a264ad39867
ssakthi <ssakthi@iu.edu>
2020-11-10 18:31:28 -0500

    Instance variable for user choice higher or lower

--- Board.py
+++ Board.py
@@ -1,18 +1,28 @@
 #Stores current state of 3 x 3 board and has functions to populate, edit, update, and determines if win or loss conditions were met
 
 class Board:
-
-    def __init__(self):
+    int choice;
+    def __init__(self, i):
+        self.choice = i
+        
 
     def populate(Deck):
         #removes top 9 cards from Deck and creates 3x3 matrix of board
 
     def updateBoard():
         #changes values on board after move is played and then prints board to console
+
     
     def compare(cardDrawn, stackPicked):
         #Sunil takes the drawn card and the player picked stack and compares the values of each and returns 
-    
+        if(cardDrawn > stackPicked):
+            cardDrawn
+        else if(cardDrawn < stackPicked):
+
+        else:
+
+
+
     def winCondition():
         #uses isEmpty() in Deck class to determine if the deck is empty or not and then looks at itself
         #to see if there is any playable spots on the board still and returns string saying "Winner!"
